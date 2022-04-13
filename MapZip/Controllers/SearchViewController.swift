60968f3a75c2fc675714b84176b73869cbc51163
choi hyunjin <hyunjin@munch-factory.com>
2022-04-13 17:00:25 +0900

    [검색 화면] tableView delegate 설정

--- MapZip/Controllers/SearchViewController.swift
+++ MapZip/Controllers/SearchViewController.swift
@@ -8,11 +8,36 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    var videoList:[String] = [""]
+    
+    let tableView: UITableView = {
+        var tableView = UITableView()
+        tableView.register(SearchTableViewCell.self, forCellReuseIdentifier: "SearchCell")
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(tableView)
+        configureTableView()
+        tableView.dataSource = self
+        tableView.delegate = self
 
         view.backgroundColor = .orange
+        
+    }
+    
+    func configureTableView() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
     }
     
 
@@ -26,4 +51,35 @@ class SearchViewController: UIViewController {
     }
     */
 
+}
+
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        
+//        cell.image = ""
+//        cell.reloadInputViews()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "최다 검색"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 44
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
 }
